6bfa33e2b75048a233e009a93e5c69793492ca44
Victor Calbucci <victor@calbucci.com>
2024-06-24 14:25:44 -0700

    Mobile Bug Fix

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const nav = document.querySelector(".hamburger-nav");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    nav.classList.toggle("nav-expanded");
 }
