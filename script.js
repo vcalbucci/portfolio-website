e1bdc88c47a52bd46da688f01a209c799276da59
Victor Calbucci <victor@calbucci.com>
2025-06-07 02:32:09 -0700

    Removed old html files

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    const nav = document.querySelector(".hamburger-nav");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-    nav.classList.toggle("nav-expanded");
-}
